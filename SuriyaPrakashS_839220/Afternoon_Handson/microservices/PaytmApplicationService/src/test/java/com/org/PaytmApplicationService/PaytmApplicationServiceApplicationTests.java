44dcd4e4c435de8a38dd88a8441dfba08ebc6c3e
Anirudh S <baakchsu.sprx77@gmail.com>
2020-12-03 20:53:43 +0530

    microservice handson

---
+++ SuriyaPrakashS_839220/Afternoon_Handson/microservices/PaytmApplicationService/src/test/java/com/org/PaytmApplicationService/PaytmApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.PaytmApplicationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytmApplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ SuriyaPrakashS_839220/Afternoon_Handson/microservices/spring-application-service-microservice/src/test/java/com/org/springapplicationservicemicroservice/SpringApplicationServiceMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.springapplicationservicemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationServiceMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
