3e899a62399859025cf6b8a38a6076d625cd1905
Anirudh S <baakchsu.sprx77@gmail.com>
2020-12-07 21:58:34 +0530

    junit test

---
+++ SuriyaPrakashS_839220/Morning_HandsOn/SpringBoot/spring-boot-junit/src/test/java/com/org/springbootjunit/Test3.java
@@ -0,0 +1,34 @@
+package com.org.springbootjunit;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers=HelloController.class)
+public class Test3 {
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private HelloService service;
+	
+	@Test
+	public void statusShouldBe202() throws Exception{
+		when(this.service.defaultMessage()).thenReturn("Welome Mock Data");
+		this.mockMvc.perform(get("/hello")).andExpect(status().isAccepted());
+	}
+	@Test
+	public void contentShouldHaveSomeMessage() throws Exception {
+		when(this.service.defaultMessage()).thenReturn("Welcome Mock Data");
+		this.mockMvc.perform(get("/hello")).andExpect(content().string(containsString("Welcome Mock Data")));
+	}
+	
+}
